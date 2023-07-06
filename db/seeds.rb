17cb656e553c763ceb94198ec12b69edeac7fdee
valerie keeton <valrie104@gmail.com>
2023-07-06 20:57:40 +0900

    created the flat model, controller and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+print "starting to make flats"
+4.times do
+  Flat.create!(
+    name: Faker::Lorem.words(number: 4),
+    address: Faker::Address.street_address,
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: Faker::Number.within(range: 50..350),
+    number_of_guests: Faker::Number.within(range: 1..10)
+  )
+end
+print "done making flats!"
